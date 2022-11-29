a01330913982c1b198a2fefdc9ab892eb52de08b
fchignolli <102098077+BrianWBurke@users.noreply.github.com>
2022-11-29 11:56:52 -0300

    att

--- docs/script/main.js
+++ docs/script/main.js
@@ -1,10 +1,13 @@
 $('.slider-principal').slick({  
     dots: true,
     infinite: true, 
-    speed: 300,
-    slidesToScroll: 1,
-    slideToShow: 1,
-    adaptiveHeight: true,
+    speed: 700,
+    // slidesToScroll: 1,
+    // slideToShow: 3,
+    // adaptiveHeight: true,
+    // variableWidth: true,
+    fade: true,
+    cssEase: 'linear',
     autoplay: true,
-    autoplaySpeed: 5000    
+    autoplaySpeed: 3000    
   });
