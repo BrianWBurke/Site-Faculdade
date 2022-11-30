eeae7a18324560b3b3eadca9db7b84027ab4a159
fchignolli <102098077+BrianWBurke@users.noreply.github.com>
2022-11-30 14:41:58 -0300

    att

--- docs/script/main.js
+++ docs/script/main.js
@@ -1,13 +1,45 @@
-$('.slider-principal').slick({  
-    dots: true,
-    infinite: true, 
-    speed: 700,
+// http://kenwheeler.github.io/slick/
+
+
+
+// $('.slider-principal').slick({  
+    // dots: true,
+    // infinite: true, 
+    // speed: 700,
+
     // slidesToScroll: 1,
     // slideToShow: 3,
     // adaptiveHeight: true,
     // variableWidth: true,
-    fade: true,
-    cssEase: 'linear',
-    autoplay: true,
-    autoplaySpeed: 3000    
-  });
+
+    // fade: true,
+    // cssEase: 'linear',
+    // autoplay: true,
+    // autoplaySpeed: 3000    
+  // });
+
+  // $('.slider-principal').slick({
+    // centerMode: true,
+    // centerPadding: '60px',
+    // slidesToShow: 3,
+    // responsive: [
+      // {
+        // breakpoint: 768,
+        // settings: {
+          // arrows: false,
+          // centerMode: true,
+          // centerPadding: '40px',
+          // slidesToShow: 3
+        // }
+      // },
+      // {
+        // breakpoint: 480,
+        // settings: {
+          // arrows: false,
+          // centerMode: true,
+          // centerPadding: '40px',
+          // slidesToShow: 1
+        // }
+      // }
+    // ]
+  // });
